ed81c00f090fa3b929dfe7901e68f9387a09d639
JoranXx1 <laster31j@gmail.com>
2023-05-01 16:10:41 +0700

    แก้ปุ่ม

--- src/pages/createreceipt.vue
+++ src/pages/createreceipt.vue
@@ -1,7 +1,7 @@
 <template>
     
-    <div class="grid px-3 gap-2 sm:grid-cols-2 sm:gap-4">
-        <a href="#" class="block max-w-full h-170 p-10 bg-white border border-gray-300 rounded-lg shadow hover:bg-gray- dark:bg-gray- dark:border-gray- dark:hover:bg-gray-200">
+    <div class="grid px-4 gap-2 sm:grid-cols-2 sm:gap-4">
+        <a href="#" class="block max-w-full h-180 p-10 bg-white border border-gray-300 rounded-lg shadow hover:bg-gray- dark:bg-gray- dark:border-gray- dark:hover:bg-gray-200">
             <section class="bg-white dark:bg-gray-900">
                 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
                 <div class="pb-4 bg-white dark:bg-gray-900">
@@ -10,7 +10,7 @@
                         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                             <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
                         </div>
-                        <input type="text" id="table-search" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for items">
+                        <input type="text" id="table-search" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="กรอกชื่อลูกค้า เพื่อทำการค้นหา">
                     </div>
                 </div>
                 <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
@@ -202,8 +202,8 @@
                             <td class="px-6 py-4">
                                 1
                             </td>
-
                         </tr>
+
                     </tbody>
                 </table>
             </div>
@@ -221,28 +221,56 @@
                                 <input type="text" name="brand" id="brand" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="" >
                             </div>
                             <div class="w-full">
-                                <label for="brand" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">สาขา (ถ้าไม่กรอกจะเป็นสำนักงานใหญ่)</label>
+                                <label for="brand" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">สาขา(ถ้าไม่กรอกจะเป็นสำนักงานใหญ่)</label>
                                 <input type="text" name="brand" id="brand" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="" >
                             </div>
                         </div>
                         <br>
-                        <div class="grid gap-4 sm:grid-cols-3 sm:gap-6">
+                        <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
                             <div class="w-full">
                                 <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">วันที่รับชำระ</label>
                                 <input type="date" id="start" name="trip-start"
                                 value=""
                                 min="2023-01-01" max="2023-12-31" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                             </div>
-                            <div class="w-full">
-                                <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ประเภทการชำระ</label>
-                                <input type="number" name="price" id="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="">
-                            </div>
                             <div>
-                                <button type="submit" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
-                                เพิ่มไกด์
-                                </button>
+                                <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">ประเภทการชำระ</label>
+                                <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option selected></option>
+                                <option value="US">เงินสด</option>
+                                <option value="CA">เงินโอนธนาคาร</option>
+                                <option value="FR">เช็คธนาคาร</option>
+                                </select>
                             </div>
-                            
+                        </div><br>
+                        <div class="grid gap-4 sm:grid-cols-3 sm:gap-6">
+                            <div>
+                                <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">หักภาษี ณ ที่จ่าย</label>
+                                <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option selected></option>
+                                <option value="US">0%</option>
+                                <option value="CA">1%</option>
+                                <option value="FR">3%</option>
+                                <option value="FR">4%</option>
+                                <option value="FR">5%</option>
+                                <option value="FR">7%</option>
+                                </select>
+                            </div>
+                            <div class="w-full">
+                                <label for="brand" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">เลขที่เช็ค</label>
+                                <input type="text" name="brand" id="brand" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="" >
+                            </div>
+                            <div class="w-full">
+                                <label for="price" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">วันที่รับชำระ</label>
+                                <input type="date" id="start" name="trip-start"
+                                value=""
+                                min="2023-01-01" max="2023-12-31" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                            </div>
+                        </div>
+                        <div class="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
+                            <button type="submit" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
+                                สร้างใบเสร็จรับเงิน
+                            </button>
                         </div>
                     </form>
                     
